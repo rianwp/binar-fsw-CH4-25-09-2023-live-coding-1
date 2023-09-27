4f58bc9c5d67f79563e7e391c23c8d56ac9610c9
rianwp <rianwp2015@gmail.com>
2023-09-28 00:50:47 +0700

    add swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // THIRD PARTY PACKAGE/MODULE
 const express = require("express")
 const morgan = require("morgan")
+const swaggerUi = require("swagger-ui-express")
+const yaml = require("js-yaml")
 
 // OUR OWN PACKAGE/MODULE
 const tourRouter = require("./routes/tourRoutes")
@@ -12,6 +14,19 @@ const app = express()
 // memodifikasi incoming request/request body ke api kita
 app.use(express.json())
 app.use(morgan("dev"))
+
+const swaggerDocument = yaml.load(
+  fs.readFileSync(
+    "./swagger/swagger.yaml",
+    "utf-8"
+  )
+)
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+)
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
